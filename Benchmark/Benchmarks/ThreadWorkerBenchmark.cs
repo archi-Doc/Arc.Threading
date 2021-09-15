54d603f9f6d031e3b3b554ec97880a22ff157387
archi-Doc <archidoc422@gmail.com>
2021-09-15 16:29:23 +0900

    Add Benchmark

---
+++ Benchmark/Benchmarks/ThreadWorkerBenchmark.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arc.Threading;
+
+namespace Benchmark.Test
+{
+    internal class ThreadWorkerBenchmark
+    {
+    }
+}
