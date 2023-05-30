86a5865e4b0f5ba0368ca1567dc7fbcb44e5c2f8
archi-Doc <archidoc422@gmail.com>
2023-05-30 12:28:07 +0900

    .

--- Arc.Threading/Lock/IAsyncLockable.cs
+++ Arc.Threading/Lock/IAsyncLockable.cs
@@ -6,11 +6,19 @@ namespace Arc.Threading;
 
 public interface IAsyncLockable : ILockable
 {
+    /// <summary>
+    /// Create a <see cref="LockStruct"/> from an <see cref="ILockable"/> object for using statement.
+    /// </summary>
+    /// <returns><see cref="LockStruct"/>.</returns>
     async Task<LockStruct> LockAsync()
     {
         var lockTaken = await this.EnterAsync().ConfigureAwait(false);
         return new(this, lockTaken);
     }
 
+    /// <summary>
+    /// Asynchronously waits to acquire an exclusive lock.
+    /// </summary>
+    /// <returns><see langword="true"/>; the lock is acquired.</returns>
     Task<bool> EnterAsync();
 }
--- Arc.Threading/Lock/ILockable.cs
+++ Arc.Threading/Lock/ILockable.cs
@@ -1,14 +1,31 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
+using System.Threading;
+
 namespace Arc.Threading;
 
 public interface ILockable
 {
+    /// <summary>
+    ///  Create a <see cref="LockStruct"/> from an <see cref="ILockable"/> object for using statement.
+    /// </summary>
+    /// <returns><see cref="LockStruct"/>.</returns>
     LockStruct Lock() => new LockStruct(this);
 
+    /// <summary>
+    /// Acquires an exclusive lock.
+    /// </summary>
+    /// <returns><see langword="true"/>; the lock is acquired.</returns>
     bool Enter();
 
+    /// <summary>
+    /// Releases the exclusive lock.
+    /// </summary>
+    /// <exception cref="SynchronizationLockException">The current thread does not own the lock.</exception>
     void Exit();
 
+    /// <summary>
+    /// Gets a value indicating whether the exclusive lock has been acquired.
+    /// </summary>
     bool IsLocked { get; }
 }
