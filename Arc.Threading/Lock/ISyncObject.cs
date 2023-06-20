9d598ebb866ac07033e042cc4528da0a4df89273
archi-Doc <archidoc422@gmail.com>
2023-06-20 20:51:05 +0900

    .

---
+++ Arc.Threading/Lock/ISyncObject.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Arc.Threading;
+
+public interface ISyncObjec
+{
+    /// <summary>
+    /// Gets the object on which to acquire the monitor lock.
+    /// </summary>
+    object SyncObject { get; }
+}
