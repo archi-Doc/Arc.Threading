5f3c698a3e9ff3d13e380ba885a03b27ecae153e
archi-Doc <archidoc422@gmail.com>
2024-11-15 17:56:43 +0900

    .

---
+++ Arc.Threading/Lock/ILockObject.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Threading;
+
+namespace Arc.Threading;
+
+/// <summary>
+/// Represents an object that provides a locking mechanism.
+/// </summary>
+public interface ILockObject
+{
+    /// <summary>
+    /// Gets the <see cref="Lock"/> object used for synchronization.
+    /// </summary>
+    Lock LockObject { get; }
+}
