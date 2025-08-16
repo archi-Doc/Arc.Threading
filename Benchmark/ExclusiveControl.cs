1f714b58885090a783443f96eadbf7ffdaaf3443
archi-Doc <archidoc422@gmail.com>
2025-08-16 10:06:28 +0900

    .

--- Benchmark/ExclusiveControl.cs
+++ Benchmark/ExclusiveControl.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Arc.Threading;
+using Nito.AsyncEx;
 
 namespace Benchmark;
 
@@ -18,6 +19,7 @@ internal static class ExclusiveControl
     private readonly static SemaphoreSlim semaphoreSlim = new(1, 1);
     private readonly static SemaphoreLock semaphoreLock = new();
     private readonly static ReaderWriterLockSlim readerWriterLockSlim = new();
+    private readonly static AsyncLock asyncLock = new();
 
     private record class Benchmark(string Name, Action EnterDelegate, Action ExitDelegate)
     {
@@ -91,6 +93,43 @@ internal static class ExclusiveControl
         }
     }
 
+    private record class Benchmark3(string Name, Func<AwaitableDisposable<IDisposable>> LockDisposable)
+    {
+        private int count;
+
+        public async Task Run()
+        {
+            var benchTimer = new BenchTimer();
+
+            for (var i = 0; i < Repetition; i++)
+            {
+                this.count = 0;
+                var tasks = new Task[Concurrency];
+                for (var t = 0; t < Concurrency; t++)
+                {
+                    tasks[t] = Task.Run(async () =>
+                    {
+                        for (var n = 0; n < N; n++)
+                        {
+                            using (await LockDisposable())
+                            {
+                                this.count++;
+                            }
+                        }
+                    });
+                }
+
+                benchTimer.Start();
+
+                await Task.WhenAll(tasks);
+
+                benchTimer.Stop();
+            }
+
+            Console.WriteLine(benchTimer.GetResult($"{this.Name}({this.count})"));
+        }
+    }
+
     static ExclusiveControl()
     {
     }
@@ -98,17 +137,19 @@ internal static class ExclusiveControl
     public static async Task Test()
     {
         var objectBenchmark = new Benchmark("object", () => Monitor.Enter(syncObject), () => Monitor.Exit(syncObject));
-        var objectBenchmark2 = new Benchmark2("object", () => { Monitor.Enter(syncObject); return Task.CompletedTask; }, () => Monitor.Exit(syncObject));
+        // var objectBenchmark2 = new Benchmark2("object", () => { Monitor.Enter(syncObject); return Task.CompletedTask; }, () => Monitor.Exit(syncObject));
         var lockBenchmark = new Benchmark("Lock", () => lockObject.Enter(), () => lockObject.Exit());
         var semaphoreSlimBenchmark = new Benchmark2("SemaphoreSlim", () => semaphoreSlim.WaitAsync(), () => semaphoreSlim.Release());
         var semaphoreLockBenchmark = new Benchmark2("SemaphoreLock", () => semaphoreLock.EnterAsync(), () => semaphoreLock.Exit());
         var readerWriterLockSlimBenchmark = new Benchmark("ReaderWriterLockSlim", () => readerWriterLockSlim.EnterWriteLock(), () => readerWriterLockSlim.ExitWriteLock());
+        var asyncLockBenchmark = new Benchmark3("AsyncLock", () => asyncLock.LockAsync());
 
         await objectBenchmark.Run();
-        await objectBenchmark2.Run();
+        // await objectBenchmark2.Run();
         await lockBenchmark.Run();
         await semaphoreSlimBenchmark.Run();
         await semaphoreLockBenchmark.Run();
         await readerWriterLockSlimBenchmark.Run();
+        await asyncLockBenchmark.Run();
     }
 }
