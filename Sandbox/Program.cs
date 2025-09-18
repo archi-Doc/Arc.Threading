041b2d560206db97c3e50f91ed944c220d0ef770
archi-Doc <archidoc422@gmail.com>
2025-09-18 12:24:17 +0900

    .

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -89,6 +89,22 @@ internal class Program
 {
     public static readonly AsyncLocal<int> AsyncLocalInstance = new();
 
+    public static int EstimateSize<TClass>()
+        where TClass : class, new()
+    {
+        const int N = 1000;
+        long before = GC.GetAllocatedBytesForCurrentThread();
+
+        for (int i = 0; i < N; i++)
+        {
+            var obj = new TClass();
+        }
+
+        long after = GC.GetAllocatedBytesForCurrentThread();
+
+        return (int)((after - before) / N);
+    }
+
     public static async Task Main(string[] args)
     {
         AppDomain.CurrentDomain.ProcessExit += (s, e) =>
@@ -104,6 +120,7 @@ internal class Program
         };
 
         Console.WriteLine("Sandbox.");
+        Console.WriteLine($"{EstimateSize<SemaphoreLock>()}");
 
         // await TestSingleTask();
         // await TestBinarySemaphore();
