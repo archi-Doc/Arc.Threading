6422815913cddaa0b3efc8478d11d0c976f8a247
archi-Doc <archidoc422@gmail.com>
2026-04-12 20:10:13 +0900

    .

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -5,13 +5,24 @@ using Arc.Threading;
 
 namespace Playground;
 
+public class TestWorker : ReusableJobWorker<ReusableThreadJob>
+{
+    public TestWorker(ThreadCoreBase? parent, Action<ReusableThreadJob>? processJob = null)
+        : base(parent, processJob)
+    {
+        var job = this.Rent();
+        this.Add(job);
+        job.Wait();
+    }
+}
+
 class Program
 {
     static async Task Main(string[] args)
     {
         Console.WriteLine("Hello World!");
 
-        var worker = new ReusableJobWorker<ReusableThreadJob>(ThreadCore.Root, x => { Thread.Sleep(1000); });
+        var worker = new TestWorker(ThreadCore.Root, x => { Thread.Sleep(1000); });
         var job = worker.Rent();
         Console.WriteLine(job.State);
         worker.Add(job);
@@ -25,7 +36,10 @@ class Program
 
         Console.WriteLine(job.State);
         Console.WriteLine(DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff"));
-        
+
+        await worker.WaitForCompletion(500);
+        Console.WriteLine(job.State);
+        Console.WriteLine(DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff"));
 
         ThreadCore.Root.Terminate();
     }
