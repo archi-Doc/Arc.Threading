47f030929927eaf90eee7f721761940bb27bb840
archi-Doc <archidoc422@gmail.com>
2026-04-20 16:01:50 +0900

    .

--- Arc.Threading/Worker/ReusableJobWorker.cs
+++ Arc.Threading/Worker/ReusableJobWorker.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using Arc.Collections;
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -49,6 +49,10 @@ class Program
         Console.WriteLine("Hello World!");
         Console.WriteLine();
 
+        var timeout = Timeout.InfiniteTimeSpan;
+        Console.WriteLine(timeout);
+        Console.WriteLine(timeout.TotalMilliseconds);
+
         await Test2();
 
         ThreadCore.Root.Terminate();
