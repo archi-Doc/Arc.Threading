f10d26c5a74effd930d9cce844764a9dade86f6c
archi-Doc <archidoc422@gmail.com>
2026-05-01 16:25:25 +0900

    Add Execution

---
+++ Arc.Threading/Execution/ExecutionHelper.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Arc.Threading;
+
+public static class ExecutionHelper
+{
+    public static ExecutionCore? ExtractCore(this CancellationToken cancellationToken)
+    {// In my opinion, CancellationToken should have been named something like TaskContext, with added features for managing parent-child dependencies and for canceling or terminating processing.
+        try
+        {
+            var cts = Unsafe.As<CancellationToken, CancellationTokenSource>(ref cancellationToken);
+            return cts as ExecutionCore;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
